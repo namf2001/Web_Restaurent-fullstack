b97f5310a2366fd13ca5ef367d845a92317f77e0
namf2001 <108163223+namf2001@users.noreply.github.com>
2024-02-25 02:38:27 +0700

    kkk

--- frontend/src/assets/images/Table/index.js
+++ frontend/src/assets/images/Table/index.js
@@ -1,10 +1,10 @@
-import TableFor2 from './TableFor-2.png';
-import TableFor3 from './TableFor-3.png';
-import TableFor4 from './TableFor-4.png';
-import TableFor5 from './TableFor-5.png';
-import TableFor6 from './TableFor-6.png';
-import TableFor8 from './TableFor-7-8.png';
-import TableFor10 from './TableFor-9-10.png';
+import TableFor2 from './tableFor-2.png';
+import TableFor3 from './tableFor-3.png';
+import TableFor4 from './tableFor-4.png';
+import TableFor5 from './tableFor-5.png';
+import TableFor6 from './tableFor-6.png';
+import TableFor8 from './tableFor-7-8.png';
+import TableFor10 from './tableFor-9-10.png';
 
 export {
     TableFor2,
